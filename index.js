6951012ff61a621a430af552bc6c2c063a08fc4d
lehuy <huyhuys297@gmail.com>
2019-07-02 13:47:08 +0700

    use lowdb save users

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ var express=require('express');
 var app = express();
 var port=3000;
 
+var low = require('lowdb') 
+var FileSync = require('lowdb/adapters/FileSync')
+var adapter = new FileSync('db.json')
+
+var db = low(adapter);
+
+db.defaults({users: [] })
+  .write();
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-var  users = [
-			{ id:0,name:'huy'},
-			{ id:1,name:'dung'}
-		];
 app.get('/',function(request,response){``
 	response.render('index',{
 		name: 'huy',
@@ -19,13 +24,13 @@ app.get('/',function(request,response){``
 	});
 });
 app.get('/users',function(req,res){
-	res.render('users/index',{users:users});
+	res.render('users/index',{users:db.get('users').value()});
 });
 
 app.get('/users/search',function(req,res){
 	var q=req.query.q;
 
-	var matchedYsers=users.filter(function(user){
+	var matchedYsers=db.get('users').value().filter(function(user){
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !=-1;
 	})
 	res.render('users/index',{users:matchedYsers,value:q});
@@ -36,7 +41,7 @@ app.get('/users/create',function(req,res){
 })
 
 app.post('/users/create',function(req,res){
-	users.push(req.body);
+	db.get('users').push(req.body).write();
 	res.redirect('/users');
 })
 
